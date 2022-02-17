4e33000ec9f25cc2cda9db0ac74a706da6232170
doebi <christoph@doeberl.at>
2022-02-17 21:59:47 +0100

    kaputt

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,48 @@
 #include <Arduino.h>
 
-#define PIN_ADC 36
+#define PIN_ADC 34
 #define PIN_MOTOR_A 16
 #define PIN_MOTOR_B 17
 
-int val = 0;
+int MAX_ADC = 4095;
+
+float val = 0;
+
+float readState() {
+  return analogRead(PIN_ADC) / MAX_ADC;
+}
+
+/*
+void setState(float newVal) {
+  val = readState();
+
+  if (val == newVal) return;
+
+  if (val < newVal) {
+    while (val < newVal) {
+      pinMode(PIN_MOTOR_A, LOW);
+      pinMode(PIN_MOTOR_B, HIGH);
+      val = readState();
+      Serial.println(val);
+      delay(20);
+    }
+  }
+
+  if (val > newVal) {
+    while (val > newVal) {
+      pinMode(PIN_MOTOR_A, HIGH);
+      pinMode(PIN_MOTOR_B, LOW);
+      val = readState();
+      Serial.println(val);
+    }
+  }
+
+  Serial.println(".");
+  //pinMode(PIN_MOTOR_A, LOW);
+  //pinMode(PIN_MOTOR_B, LOW);
+}
+*/
+
 
 void setup() {
   Serial.begin(115200);
@@ -16,21 +54,20 @@ void setup() {
   pinMode(PIN_MOTOR_B, OUTPUT);
 }
 
-// Standard Arduino function that is called in an endless loop after setup
 void loop() {
 
   // A
   Serial.println("A");
-  digitalWrite(PIN_MOTOR_A, LOW);
-  digitalWrite(PIN_MOTOR_B, HIGH);
+  pinMode(PIN_MOTOR_A, LOW);
+  pinMode(PIN_MOTOR_B, HIGH);
+  //setState(1);
   delay(2000);
 
   // B
   Serial.println("B");
-  digitalWrite(PIN_MOTOR_A, HIGH);
-  digitalWrite(PIN_MOTOR_B, LOW);
+  pinMode(PIN_MOTOR_A, HIGH);
+  pinMode(PIN_MOTOR_B, LOW);
+  //setState(0.5);
   delay(2000);
 
-  //val = analogRead(PIN_ADC);
-  //Serial.println(val);
 }
